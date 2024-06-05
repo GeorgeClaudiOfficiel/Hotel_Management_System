af59d45ad94b22e93b8dee5581dbdd754004f8a5
GeorgeClaudiOfficiel <georgeclaudiofficiel@gmail.com>
2024-06-05 20:57:52 +0300

    To be updated

--- booking.py
+++ booking.py
@@ -1,5 +1,8 @@
 class Booking:
     def __init__(self, room, guest, booking_id, booking_date, booking_status, check_in_time, check_out_time):
+        """
+        This method initializes the attributes shown above for the 'Booking' instance
+        """
         self.booking_id = booking_id
         self.booking_date = booking_date
         self.booking_status = booking_status
--- guest.py
+++ guest.py
@@ -1,5 +1,8 @@
 class Guest:
     def __init__(self, guest_name, guest_id, guest_mobile):
+        """
+         This method initializes the attributes shown above for the 'Guest' instance
+         """
         self.guest_name = guest_name
         self.guest_id = guest_id
         self.guest_mobile = guest_mobile
