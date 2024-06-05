45cc6eaf1300738b0639f5557bd2d055d2305ec4
GeorgeClaudiOfficiel <georgeclaudiofficiel@gmail.com>
2024-06-05 17:54:17 +0300

    To be updated

---
+++ room.py
@@ -0,0 +1,8 @@
+class Room:
+    def __innit__(self, room_number, room_type, price):
+        self.room_number = room_number
+        self.room_type = room_type
+        self.price = price
+        self.is_available = True
+        
+    def update_room(self, room_number = None, room_type = None)
