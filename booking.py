99e2200b2de6e00ab7d0603af11a7e4f1b00191a
GeorgeClaudiOfficiel <georgeclaudiofficiel@gmail.com>
2024-06-05 18:40:52 +0300

    To be update

---
+++ booking.py
@@ -0,0 +1,11 @@
+class Booking:
+    def __innit__(self, room, guest, booking_id, booking_date, booking_status, check_in_time, check_out_time):
+        self.booking_id = booking_id
+        self.booking_date = booking_date
+        self.booking_status = booking_status
+        self.room = room
+        self.guest = guest
+        self.check_in_time = check_in_time
+        self.check_out_time = check_out_time
+        
+        
